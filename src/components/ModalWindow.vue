a7c521f154f21ecf400046ad91c2054ec2c3b668
Павел Андреевич <71135153+PavelIlyin@users.noreply.github.com>
2021-06-15 22:31:18 +1000

    lesson-6

---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,43 @@
+<template>
+  <div :class="[$style.wrapper]">
+    <div class="overlay"></div>
+    <header>Name</header>
+    <PaymentForm  v-if="name === 'PaymentForm'" :ids="settings && settings.id"/>
+    <button @click="onClose">Close</button>
+  </div>
+</template>
+
+<script>
+import PaymentForm from './PaymentForm';
+export default {
+  components: {
+    PaymentForm,
+  },
+  data() {
+    return {
+    };
+  },
+  props: {
+    name: String,
+    settings: Object,
+  },
+  methods: {
+
+    onClose () {
+      this.$modal.close()      
+    }
+  },
+};
+</script>
+
+<style lang="scss" module>
+.wrapper {
+  border: 1px solid black;
+  width: 80%;
+  position: absolute;
+  top: 170px;
+  background-color: rgba(250, 250, 250, 1);
+  z-index: 100;
+ 
+}
+</style>
---
+++ src/page/PageDashboard.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <main :class="[$style.payment]">
+      <!--<button :class="[$style.payment__btn]" @click="showPaymentForm">
+        {{ visible ?  "HIDE -" : "ADD NEW COST +" }}
+      </button>
+      <ModalWindow modal='paymentform'/>
+      <PaymentForm v-show="visible" />-->
+      <PaymentsList  />
+    </main>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapMutations } from "vuex";
+
+import PaymentsList from "../components/PaymentsList";
+
+export default {
+  components: {
+    PaymentsList,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    ...mapActions(["fetchData"]),
+    ...mapMutations(["showForm"]),
+  },
+  mounted() {
+    this.fetchData();
+    console.log(this.id)
+  },
+};
+</script>
+
+<style lang="scss" module>
+.payment__btn {
+  padding: 8px;
+  background-color: rgba(16, 148, 60, 0.863);
+  color: rgb(255, 255, 255);
+  width: 180px;
+  overflow: hidden;
+  border: none;
+  border-radius: 5px;
+}
+.payment__btn:hover {
+  background-color: rgba(16, 134, 55, 0.863);
+  box-shadow: 1px 1px 3px rgb(1, 77, 14);
+}
+
+.payment__btn:active {
+  background-color: rgba(48, 109, 69, 0.863);
+  box-shadow: inset 1px 1px 5px rgb(1, 56, 10);
+}
+</style>
