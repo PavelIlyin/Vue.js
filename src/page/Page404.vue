e8f1aa0cc17b9b620bdbb129126eb5439500e867
Павел Андреевич <71135153+PavelIlyin@users.noreply.github.com>
2021-07-16 23:57:59 +1000

    lesson-8

---
+++ src/page/Page404.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-h3 text-center pa-6">
+    404
+    <p class="text-h5">Not Found</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
